17823f5645e135bb3586ba437d594b63b6794999
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-13 23:47:09 -0300

    event bind method

--- contactmanager/src/components/contact/Contact.jsx
+++ contactmanager/src/components/contact/Contact.jsx
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 
 class Contact extends Component {
+  state = {};
+
+  onShowClick = (id, e) => {
+    console.log(id);
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="container">
         <div className="card card-body mb-3">
-          <h4>{name}</h4>
+          <h4>
+            {name}{" "}
+            <i
+              className="fas fa-sort-down"
+              onClick={this.onShowClick.bind(this, name)}
+            />
+          </h4>
           <ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
