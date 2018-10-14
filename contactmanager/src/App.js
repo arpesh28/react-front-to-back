864885946679b06051d7e57ee4473f1d33a82b2b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-13 22:37:15 -0300

    Header Arrow Function

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/contact/Contact";
+import Header from "./components/header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header />
+        <Contact />
       </div>
     );
   }
