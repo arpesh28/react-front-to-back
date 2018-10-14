9043d5f4d01b30fe1fd926a377d185532cc1dd9c
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 01:15:18 -0300

    defaultValue

--- contactmanager/src/components/contacts/AddContact.1.jsx
+++ contactmanager/src/components/contacts/AddContact.1.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 
 class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   onSubmit = e => {
     e.preventDefault();
     console.log(this.state);
   };
 
-  static defaultProps = {
-    name: "Yang Ricardo",
-    email: "yangricardo@aluno.puc-rio.br",
-    phone: "+55 21 99556-0615"
-  };
-
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
@@ -26,7 +28,8 @@ class AddContact extends Component {
                 name="name"
                 className="form-control form-control-lg"
                 placeholder="Insira o nome... "
-                defaultValue={name}
+                value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -36,7 +39,8 @@ class AddContact extends Component {
                 name="email"
                 className="form-control form-control-lg"
                 placeholder="Insira o email... "
-                defaultValue={email}
+                value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -46,7 +50,8 @@ class AddContact extends Component {
                 name="phone"
                 className="form-control form-control-lg"
                 placeholder="Insira o telefone... "
-                defaultValue={phone}
+                value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
