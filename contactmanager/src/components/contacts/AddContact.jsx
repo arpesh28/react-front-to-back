b010ea85e040bef5e5b817f17f5577c75909dde6
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 02:44:31 -0300

    toggle add contact

--- contactmanager/src/components/contacts/AddContact.jsx
+++ contactmanager/src/components/contacts/AddContact.jsx
@@ -6,7 +6,8 @@ class AddContact extends Component {
   state = {
     name: "",
     email: "",
-    phone: ""
+    phone: "",
+    addContact: false
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -34,6 +35,7 @@ class AddContact extends Component {
 
   render() {
     const { name, email, phone } = this.state;
+    const { addContact } = this.state;
 
     return (
       <Consumer>
@@ -42,38 +44,60 @@ class AddContact extends Component {
 
           return (
             <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
-              <div className="card-body">
-                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <TextInputGroup
-                    label="Nome"
-                    name="name"
-                    placeholder="Insira o nome... "
-                    value={name}
-                    onChange={this.onChange}
-                  />
-                  <TextInputGroup
-                    label="Telefone"
-                    name="phone"
-                    placeholder="Insira o Telefone... "
-                    value={phone}
-                    onChange={this.onChange}
-                  />
-                  <TextInputGroup
-                    label="Email"
-                    name="email"
-                    type="email"
-                    placeholder="Insira o email... "
-                    value={email}
-                    onChange={this.onChange}
-                  />
-                  <input
-                    type="submit"
-                    value="Add Contact"
-                    className="btn btn-light btn-block"
-                  />
-                </form>
+              <div
+                className="card-header"
+                style={{ cursor: "pointer" }}
+                onClick={() =>
+                  this.setState({
+                    addContact: !this.state.addContact
+                  })
+                }
+              >
+                Add Contact
+                <i
+                  className={
+                    "fas fa-" + (addContact ? "minus" : "plus") + "-circle"
+                  }
+                  style={{
+                    cursor: "pointer",
+                    float: "right",
+                    color: "#076BFF"
+                  }}
+                />
               </div>
+              {addContact ? (
+                <div className="card-body">
+                  <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                    <TextInputGroup
+                      label="Nome"
+                      name="name"
+                      placeholder="Insira o nome... "
+                      value={name}
+                      onChange={this.onChange}
+                    />
+                    <TextInputGroup
+                      label="Telefone"
+                      name="phone"
+                      placeholder="Insira o Telefone... "
+                      value={phone}
+                      onChange={this.onChange}
+                    />
+                    <TextInputGroup
+                      label="Email"
+                      name="email"
+                      type="email"
+                      placeholder="Insira o email... "
+                      value={email}
+                      onChange={this.onChange}
+                    />
+                    <input
+                      type="submit"
+                      value="Add Contact"
+                      className="btn btn-light btn-block"
+                    />
+                  </form>
+                </div>
+              ) : null}
             </div>
           );
         }}
