4db73b41a1d163e42a4dd5aa29f8c057cfaed853
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-13 23:58:03 -0300

    toggling contact info

--- contactmanager/src/components/contact/Contact.jsx
+++ contactmanager/src/components/contact/Contact.jsx
@@ -8,6 +8,8 @@ class Contact extends Component {
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="container">
         <div className="card card-body mb-3">
@@ -20,10 +22,12 @@ class Contact extends Component {
               }
             />
           </h4>
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
+          {showContactInfo ? (
+            <ul className="list-group">
+              <li className="list-group-item">Email: {email}</li>
+              <li className="list-group-item">Phone: {phone}</li>
+            </ul>
+          ) : null}
         </div>
       </div>
     );
