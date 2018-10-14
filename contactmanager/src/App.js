e76d0d3d50c1a4d5043a17e637986bab502a0d2e
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 00:27:58 -0300

    getting contacts from Context API provider

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/header/Header";
+import { Provider } from "./providers/contact";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -9,12 +10,14 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contacts />
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
--- contactmanager/src/components/contacts/Contacts.jsx
+++ contactmanager/src/components/contacts/Contacts.jsx
@@ -1,31 +1,9 @@
 import React, { Component } from "react";
 
 import Contact from "../contact/Contact";
+import { Consumer } from "../../providers/contact";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Yang Ricardo Barcellos Miranda",
-        email: "yang@mail.com",
-        phone: "995560615"
-      },
-      {
-        id: 2,
-        name: "Ricardo Barcellos Miranda",
-        email: "ricardo@mail.com",
-        phone: "995650615"
-      },
-      {
-        id: 3,
-        name: "Vera Lúcia Alves de Oliveira",
-        email: "vera@mail.com",
-        phone: "995560620"
-      }
-    ]
-  };
-
   deleteContact = id => {
     const { contacts } = this.state;
     const newContacts = contacts.filter(contact => contact.id !== id);
@@ -33,17 +11,26 @@ class Contacts extends Component {
   };
 
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            onDeleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  onDeleteClickHandler={this.deleteContact.bind(
+                    this,
+                    contact.id
+                  )}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
