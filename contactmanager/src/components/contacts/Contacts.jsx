c1d27d524aef83328c7234b22d3ceee9a6a408aa
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-13 23:33:37 -0300

    state 2

--- contactmanager/src/components/contacts/Contacts.jsx
+++ contactmanager/src/components/contacts/Contacts.jsx
@@ -3,43 +3,35 @@ import React, { Component } from "react";
 import Contact from "../contact/Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Yang Ricardo Barcellos Miranda",
-          email: "yang@mail.com",
-          phone: "995560615"
-        },
-        {
-          id: 2,
-          name: "Ricardo Barcellos Miranda",
-          email: "ricardo@mail.com",
-          phone: "995650615"
-        },
-        {
-          id: 3,
-          name: "Vera Lúcia Alves de Oliveira",
-          email: "vera@mail.com",
-          phone: "995560620"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Yang Ricardo Barcellos Miranda",
+        email: "yang@mail.com",
+        phone: "995560615"
+      },
+      {
+        id: 2,
+        name: "Ricardo Barcellos Miranda",
+        email: "ricardo@mail.com",
+        phone: "995650615"
+      },
+      {
+        id: 3,
+        name: "Vera Lúcia Alves de Oliveira",
+        email: "vera@mail.com",
+        phone: "995560620"
+      }
+    ]
+  };
 
   render() {
     const { contacts } = this.state;
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
