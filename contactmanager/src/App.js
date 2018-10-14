b40c7b94a5ec4854fdee5ea1c394151666d7a272
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 03:08:02 -0300

    routing pages

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/layout/Header";
+import About from "./components/layout/About";
 
 import AddContact from "./components/contacts/AddContact";
 import Contacts from "./components/contacts/Contacts";
@@ -14,13 +16,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Meus Contatos" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Meus Contatos" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
---
+++ contactmanager/src/components/layout/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">Sobre Meus Contatos</h1>
+      <p className="lead">Um simples gerenciador de contatos</p>
+      <p className="text-secondary">Versão 1.0.0</p>
+    </div>
+  );
+};
