bceb34c4f04cd05d3681c803798f2a11b8b19384
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-15 18:59:19 -0300

    settings changing on local storage

--- firebase_client_panel/src/components/settings/Settings.jsx
+++ firebase_client_panel/src/components/settings/Settings.jsx
@@ -9,11 +9,26 @@ import {
 } from "../../store/actions/settingsActions";
 
 class Settings extends Component {
+  disableBalanceOnAddChange = () => {
+    const { setDisableBalanceOnAdd } = this.props;
+    setDisableBalanceOnAdd();
+  };
+
+  disableBalanceOnEditChange = () => {
+    const { setDisableBalanceOnEdit } = this.props;
+    setDisableBalanceOnEdit();
+  };
+
+  allowRegistrationChange = () => {
+    const { setAllowRegistration } = this.props;
+    setAllowRegistration();
+  };
+
   render() {
     const {
-      setDisableBalanceOnAdd,
-      setDisableBalanceOnEdit,
-      setAllowRegistration
+      disableBalanceOnAdd,
+      disableBalanceOnEdit,
+      allowRegistration
     } = this.props.settings;
     return (
       <div>
@@ -23,6 +38,40 @@ class Settings extends Component {
               <i className="fas fa-arrow-circle-left" />
               Back to Dashboard
             </Link>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Edit Settings</div>
+          <div className="card-body">
+            <form onSubmit="">
+              <div className="form-group">
+                <label> Allow Registration </label>{" "}
+                <input
+                  type="checkbox"
+                  name="allowRegistration"
+                  checked={!!allowRegistration}
+                  onChange={this.allowRegistrationChange}
+                />
+              </div>
+              <div className="form-group">
+                <label> Disable Balance On Add </label>{" "}
+                <input
+                  type="checkbox"
+                  name="disableBalanceOnAdd"
+                  checked={!!disableBalanceOnAdd}
+                  onChange={this.disableBalanceOnAddChange}
+                />
+              </div>
+              <div className="form-group">
+                <label> Disable Balance On Edit </label>{" "}
+                <input
+                  type="checkbox"
+                  name="disableBalanceOnEdit"
+                  checked={!!disableBalanceOnEdit}
+                  onChange={this.disableBalanceOnEditChange}
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
