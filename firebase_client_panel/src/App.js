aa6adb08a4fb25b25eb3352b78a265d41c3d846d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-15 19:46:11 -0300

    creating new account and verifying if already exists

--- firebase_client_panel/src/App.js
+++ firebase_client_panel/src/App.js
@@ -10,6 +10,7 @@ import AddClient from "./components/clients/AddClient";
 import DetailsClient from "./components/clients/DetailsClient";
 import EditClient from "./components/clients/EditClient";
 import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Settings from "./components/settings/Settings";
 
 class App extends Component {
@@ -50,6 +51,11 @@ class App extends Component {
                   exact
                   path="/login"
                   component={UserIsNotAuthenticated(Login)}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  component={UserIsNotAuthenticated(Register)}
                 />
               </Switch>
             </div>
