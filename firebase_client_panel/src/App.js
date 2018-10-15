b07c661606bc5d28eecc1f6bf77161007e03522a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-15 16:27:41 -0300

    install redux-auth-wrapper

--- firebase_client_panel/src/App.js
+++ firebase_client_panel/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
 import AppNavBar from "./components/layout/AppNavBar";
 import Dashboard from "./components/layout/Dashboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -20,11 +20,31 @@ class App extends Component {
             <AppNavBar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/client/:id" component={DetailsClient} />
-                <Route exact path="/login" component={Login} />
+                <Route
+                  exact
+                  path="/"
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path="/client/add"
+                  component={UserIsAuthenticated(AddClient)}
+                />
+                <Route
+                  exact
+                  path="/client/edit/:id"
+                  component={UserIsAuthenticated(EditClient)}
+                />
+                <Route
+                  exact
+                  path="/client/:id"
+                  component={UserIsAuthenticated(DetailsClient)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
               </Switch>
             </div>
           </div>
