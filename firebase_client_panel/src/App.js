5d19a9cbb6befd614e306faa4a482abde49ebda7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 22:21:58 -0300

    app nav bar fix

--- firebase_client_panel/src/App.js
+++ firebase_client_panel/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import AppNavBar from "./components/layout/AppNavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavBar />
+          <div className="container">
+            <h1>Hello</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- firebase_client_panel/src/components/layout/AppNavBar.jsx
+++ firebase_client_panel/src/components/layout/AppNavBar.jsx
@@ -16,16 +16,16 @@ class AppNavBar extends Component {
             data-target="#navbarMain"
           >
             <span className="navbar-toggler-icon" />
-            <div className="collapse navbar-collapse" id="navbarMain">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link to="/" className="nav-link">
-                    Dashboard
-                  </Link>
-                </li>
-              </ul>
-            </div>
           </button>
+          <div className="collapse navbar-collapse" id="navbarMain">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
