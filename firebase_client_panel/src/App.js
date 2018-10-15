8598862d97c9db105199b7e66a816823a0dda121
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-14 23:22:44 -0300

    react redux firebase config

--- firebase_client_panel/src/App.js
+++ firebase_client_panel/src/App.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 
 import AppNavBar from "./components/layout/AppNavBar";
+import Dashboard from "./components/layout/Dashboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <AppNavBar />
-          <div className="container">
-            <h1>Hello</h1>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <AppNavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ firebase_client_panel/src/store/store.js
@@ -0,0 +1,56 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
+import { reduxFirestore, firestoreReducer } from "redux-firestore";
+
+// Reducers
+// @todo
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbFcitt9E4kvuxdDEQsiIFGSUuYaI2ObY",
+  authDomain: "reactclientpanel-c8f9c.firebaseapp.com",
+  databaseURL: "https://reactclientpanel-c8f9c.firebaseio.com",
+  projectId: "reactclientpanel-c8f9c",
+  storageBucket: "reactclientpanel-c8f9c.appspot.com",
+  messagingSenderId: "839649464851"
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Init Firestore
+const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase) // <- needed if using firestore
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer // <- needed if using firestore
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store with reducers and initial state
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
